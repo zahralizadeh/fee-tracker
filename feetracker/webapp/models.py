6d024b44bdaa590318bcdba9283820d699ffd6d4
Zahra alizadeh <zahra.alizadeh@gmail.com>
2019-08-25 20:36:14 +0430

    model created and migrated to database

--- feetracker/webapp/models.py
+++ feetracker/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class PropertyFile(models.Model):
+    offertype = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 255)
+    area = models.IntegerField()
+    price1 = models.BigIntegerField()
+    price2 = models.BigIntegerField()
+    rooms = models.IntegerField()
+    age = models.IntegerField()

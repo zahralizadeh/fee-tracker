f2a7a94cf68c4478996d55a19dc903d8262d3067
zahra <zahra.alizadeh@gmail.com>
2019-09-01 22:24:59 +0430

    token model

--- feetracker/webapp/views.py
+++ feetracker/webapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+import requests
+from views import activationcode
+
 
 # Create your views here.
+
+def manageRequestRegistrationCode(request):
+    
+    
+@csrf_exempt
+def register (request):
+
+    if request.POST.haskey('requestcode'):
+        #the client wants to register so send him a registration code
+        return manageRequestRegistrationCode(request)
+    elif request.Get.has_key('code'):
+        #the client has clicke on activation link
+        return manageClickingActivationLink()
+    else:
+        #unknows request
+        return manageUnknownRequest()
+    
+

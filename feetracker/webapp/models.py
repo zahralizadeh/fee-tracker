626e4393f0b0aa1363d6980ea7e62ccbed3ca48e
zahra <zahra.alizadeh@gmail.com>
2019-09-01 22:24:59 +0430

    token model

--- feetracker/webapp/models.py
+++ feetracker/webapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class PropertyFile(models.Model):
     offertype = models.CharField(max_length = 50)
@@ -16,5 +17,15 @@ class PropertyFile(models.Model):
 class Token(models.Model):
     user = models.OneToOneField(User,on_delete = models.CASCADE)
     token = models.CharField(max_length = 48)
+    def __str__(self):
+        return "{}-token".format(self.user)
+
+class activationcode(models.Model):
+    username = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 120)
+    password = models.CharField(max_length = 50)
+    code = models.CharField(max_length = 28)
+    time = models.DateTimeField()
+
 
 

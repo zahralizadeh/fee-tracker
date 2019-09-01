53a6dfb6a626f52e31e5824b4169e3d397426f83
zahralizadeh <zahra.alizadeh@gmail.com>
2019-09-01 15:29:26 +0430

    Update samplesettings.py
    updated

--- feetracker/feetracker/samplesettings.py
+++ feetracker/feetracker/samplesettings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6#@w$l3n6pb*9fdh6yika1rs!3i^9bsz2f13n7)=dvr4lkxh86'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

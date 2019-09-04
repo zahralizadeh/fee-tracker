154725ceaa9d73da21687c0e55a80bb5732a3443
U-TPPH\z.alizadeh <z.alizadeh@40_ALIZADEH.tpph.local>
2019-09-04 12:35:43 +0430

    register template created

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     #url(r'',include ('webapp.urls')),
+    url(r'^register/', views.register, name='register'),
+    url(r'', views.register, name='register'),
 ]

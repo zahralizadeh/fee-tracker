c165a49face2913c0692b5ced3cc3199f8a87657
zahra <zahra.alizadeh@gmail.com>
2019-09-09 20:06:18 +0430

    propertyfile mdel deleted from web app

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import PropertyFile, Token, activationcode
+from .models import Token, activationcode
 # Register your models here.
 
-admin.site.register(PropertyFile)
 admin.site.register(Token)
 admin.site.register(activationcode)

bdf44456cfb47bae7f511c8e6be6f393279f59a3
zahra <zahra.alizadeh@gmail.com>
2019-09-09 21:46:39 +0430

    ScrapeLog model created

--- webscraper/views.py
+++ webscraper/views.py
@@ -1,3 +1,8 @@
+import requests
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def collectdata(request):
+    return HttpResponse('Hi...we are going to Store information about the house you want in database!')
+

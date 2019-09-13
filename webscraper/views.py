982b02e7266e0866c82e3ae7ff0548d09fe2b51b
zahra <zahra.alizadeh@gmail.com>
2019-09-14 00:35:59 +0430

    first step of creating loop for collecting data

--- webscraper/views.py
+++ webscraper/views.py
@@ -5,16 +5,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Scrape, PropertyFile
 from datetime import datetime
-from django.db.models import Max    
+from django.db.models import Max,Min 
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 def collectdata(request):
     logger.debug("----def collectdata ----->  is running")
-    last_update = PropertyFile.objects.all().aggregate(Max('publishdate'))
-    logger.debug("----def collectdata last update is on  ----->  %s"%last_update)
-    
+    last_property = PropertyFile.objects.all().aggregate(Max('publishdate'))
+    first_property = PropertyFile.objects.all().aggregate(Min('publishdate'))
+
+    logger.debug("----def collectdata last saved property is published on  ----->  %s"%last_property)
+    logger.debug("----def collectdata last saved property is published on  ----->  %s"%first_property)
+
     scrapeIhomeBuy = Scrape(scrapetype= 'خرید-فروش')
     if scrapeIhomeBuy.startscraping():
         scrapeIhomeBuy.save()

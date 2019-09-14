dd2f434cc40b05360c30d31b0b11c4d3cd05a017
zahralizadeh <zahra.alizadeh@gmail.com>
2019-09-14 11:40:55 +0430

    requirements updated

--- webscraper/views.py
+++ webscraper/views.py
@@ -12,14 +12,14 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 def collectdata(request):
     logger.debug("----def collectdata ----->  is running")
-    last_property = PropertyFile.objects.all().aggregate(Max('publishdate'))
+    last_property_time = PropertyFile.objects.all().aggregate(Max('publishdate'))
     first_property = PropertyFile.objects.all().aggregate(Min('publishdate'))
     
     logger.debug("----def collectdata last saved property is published on  ----->  %s"%last_property)
     logger.debug("----def collectdata last saved property is published on  ----->  %s"%first_property)
 
     scrapeIhomeBuy = Scrape(scrapetype= 'خرید-فروش')
-    if scrapeIhomeBuy.startscraping():
+    if scrapeIhomeBuy.startscraping_update():
         scrapeIhomeBuy.save()
     return HttpResponse('Hi...we are going to Store information about the house you want in database!')
 

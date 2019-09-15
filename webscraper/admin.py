e0fe9595b15d11aabb54113b721da6698705e49e
zahra <zahra.alizadeh@gmail.com>
2019-09-15 19:27:35 +0430

    admin tables improved; list_display overrided

--- webscraper/admin.py
+++ webscraper/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import PropertyFile, Scrape
 
 # Register your models here.
-admin.site.register(PropertyFile)
-admin.site.register(Scrape)
+class PropertyFileAdmin (admin.ModelAdmin):
+    list_display = ['offertype', 'location', 'area', 'price1', 'price2', 'rooms', 'age', 'publishdate' ]
+
+class ScrapeAdmin (admin.ModelAdmin):
+    list_display = ['endTime','status', 'scrapetype', 'currnetrecord','pagenumber','last_update_time', 'site']
+    
+
+admin.site.register(PropertyFile,PropertyFileAdmin)
+admin.site.register(Scrape,ScrapeAdmin)
 

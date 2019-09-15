902c80b5b92a8b5416dfe97b0a40e718edb2be81
zahra <zahra.alizadeh@gmail.com>
2019-09-15 19:27:35 +0430

    admin tables improved; list_display overrided

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Token, activationcode
-# Register your models here.
 
-admin.site.register(Token)
-admin.site.register(activationcode)
+
+# Register your models here.
+class TokenAdmin (admin.ModelAdmin):
+    list_display = ['user', 'token']
+
+class activationcodeAdmin (admin.ModelAdmin):
+    list_display = ['username', 'email','code','time']
+
+admin.site.register(Token, TokenAdmin)
+admin.site.register(activationcode, activationcodeAdmin)

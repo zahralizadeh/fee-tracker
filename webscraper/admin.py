4b618e3825866c55d0cad697f1c7bd04e6fcb136
zahra <zahra.alizadeh@gmail.com>
2019-09-21 23:51:21 +0430

    prediction version 1 worked

--- webscraper/admin.py
+++ webscraper/admin.py
@@ -4,9 +4,11 @@ from .models import PropertyFile, Scrape
 # Register your models here.
 class PropertyFileAdmin (admin.ModelAdmin):
     list_display = ['offertype', 'location', 'area', 'price1', 'price2', 'rooms', 'age', 'publishdate' ]
+    search_fields = ('offertype', 'location' )
 
 class ScrapeAdmin (admin.ModelAdmin):
     list_display = ['endTime','status', 'scrapetype', 'currnetrecord','pagenumber','last_update_time', 'site']
+    search_fields = ('last_update_time', 'status', 'scrapetype' )
     
 
 admin.site.register(PropertyFile,PropertyFileAdmin)

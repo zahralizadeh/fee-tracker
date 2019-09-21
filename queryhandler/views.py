6b0f9a29db5e717c576e04061b55d425e8aa7bbd
zahra <zahra.alizadeh@gmail.com>
2019-09-21 23:51:21 +0430

    prediction version 1 worked

--- queryhandler/views.py
+++ queryhandler/views.py
@@ -2,6 +2,8 @@
 
 import logging
 import requests
+import numpy as np
+from sklearn import tree
 from django.http import HttpResponse
 from json import JSONEncoder,dumps
 from django.shortcuts import render
@@ -24,7 +26,10 @@ def query_price(request):
     #TODO: validate user input
 
     data = PropertyFile.objects.filter(offertype=this_offertype,location__contains=this_location).order_by('-publishdate')
-    #TODO: filter data with other criterea
+    #TODO: filter data with other criterea due to data.count()
+    #TODO: prediction model. save result of prediction
+    #TODO: search prediction history before start prediction
+    #TODO: token provided or something to prevent robots
 
     if data.count()< 50:
         return JsonResponse({
@@ -34,19 +39,30 @@ def query_price(request):
     count = data.count() if data.count()<150 else 150  # max 150 recordes are needed for prediction
     x = []
     y = []
+    #asnwer =
     if this_offertype == '1':   # load input and output lists for prediction with last 150 record in database( MAX)
         for j in range(0, count):
-            x.append([data[j].location, data[j].area, data[j].rooms,data[j].age])
+            x.append([data[j].area, data[j].rooms,data[j].age])
             y.append(data[j].price1)
     elif this_offertype == '2':
         for j in range(0, count):
-            x.append([data[j].location, data[j].area, data[j].rooms,data[j].age])
-            y.append(data[j].price1,data[j].price2)
-
+            x.append([data[j].area, data[j].rooms,data[j].age])
+            y.append([data[j].price1,data[j].price2])
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(x,y)
+    answer = clf.predict([[this_area,this_rooms,this_age]])
+    if this_offertype == '1':
+        return JsonResponse({
+            'count' : '%i'%data.count(),
+            'answer': 'I predict its cost as %i tomans'%answer[0],
+            }, encoder=JSONEncoder)
+    else:
+        return JsonResponse({
+            'count' : '%i'%data.count(),
+            'answer': 'I predict its cost as %i tomans for deposit and %i tomans as monthly rent'%(answer[0][1],answer[0][0]),
+            }, encoder=JSONEncoder)
     #logger.debug('%s %s %s %s %s'%(this_offertype,this_location,this_area,this_age,this_rooms))
-    return JsonResponse({
-        'number of data': '%i %i'%(len(x),len(y)),
-    }, encoder=JSONEncoder)
+    
 
 
 @csrf_exempt
